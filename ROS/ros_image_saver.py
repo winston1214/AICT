8acab04610ed537057242fe1b7e563f97e2a6a80
winston1214 <47775179+winston1214@users.noreply.github.com>
2020-12-10 00:07:56 +0900

    camera image saver in ros

---
+++ ROS/ros_image_saver.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python3
+
+
+# Using this OpenCV2 tutorial for saving Images:
+# http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_gui/py_image_display/py_image_display.html
+
+# rospy for the subscriber
+import rospy
+# ROS Image message
+from sensor_msgs.msg import Image
+# ROS Image message -> OpenCV2 image converter
+from cv_bridge import CvBridge, CvBridgeError
+# OpenCV2 for saving an image
+import cv2
+
+# Instantiate CvBridge
+bridge = CvBridge()
+
+def image_callback(msg):
+    # print("Received an image!")
+    try:
+        # Convert your ROS Image message to OpenCV2
+        cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+    except CvBridgeError as e:
+        print(e)
+    else:
+        # Save your OpenCV2 image as a jpeg 
+        cv2.imwrite('camera_image.jpg', cv2_img)
+
+def main():
+    rospy.init_node('image_listener')
+    # Define your image topic
+    image_topic = "/pylon_camera_node1/image_raw"
+    # Set up your subscriber and define its callback
+    rospy.Subscriber(image_topic, Image, image_callback)
+    # Spin until ctrl + c
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
